96a0f414c79412b2ea6e2284822bb6def7861aab
parshantrampuriya <parshantrampuriya@gmail.com>
2026-03-21 05:50:38 +0530

    login.js

--- js/login.js
+++ js/login.js
@@ -11,10 +11,21 @@ document.getElementById("loginBtn").addEventListener("click", async () => {
     let email = document.getElementById("email").value;
     let password = document.getElementById("password").value;
 
+    if (!email || !password) {
+        alert("Please enter email and password");
+        return;
+    }
+
     try {
         await signInWithEmailAndPassword(auth, email, password);
-        alert("Login Successful ✅");
-        window.location.href = "home.html";
+
+        /* 🔥 SHOW SUCCESS POPUP */
+        document.getElementById("successPopup").style.display = "flex";
+
+        /* 🔥 AUTO REDIRECT AFTER 1 SECOND */
+        setTimeout(() => {
+            window.location.href = "home.html";
+        }, 1000);
 
     } catch (error) {
         alert(error.message);
@@ -26,7 +37,7 @@ document.getElementById("goSignupBtn").addEventListener("click", () => {
     window.location.href = "signup.html";
 });
 
-/* OPEN POPUP */
+/* OPEN FORGOT PASSWORD POPUP */
 document.getElementById("forgotBtn").addEventListener("click", () => {
     document.getElementById("forgotPopup").style.display = "flex";
 });
@@ -48,6 +59,7 @@ document.getElementById("resetBtn").addEventListener("click", async () => {
 
     try {
         await sendPasswordResetEmail(auth, email);
+
         alert("Reset link sent to your email 📩");
 
         document.getElementById("forgotPopup").style.display = "none";
