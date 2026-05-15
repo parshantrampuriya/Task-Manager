8b5f83012bc48d7e68e5444722818d5ec4a45329
parshantrampuriya <parshantrampuriya@gmail.com>
2026-05-15 21:49:28 +0530

    Update fishbone.js

--- js/fishbone.js
+++ js/fishbone.js
@@ -1,3 +1,34 @@
-// FILE NAME: fishbone.js
+/* REPLACE YOUR COMPLETE fishbone.js WITH THIS */
 
-console.log("Fishbone Diagram Ready");
+const textareas = document.querySelectorAll("textarea");
+
+textareas.forEach((textarea)=>{
+
+    function autoResize(){
+
+        textarea.style.height = "auto";
+        textarea.style.height = textarea.scrollHeight + "px";
+
+        updateFishSize();
+    }
+
+    textarea.addEventListener("input",autoResize);
+
+    autoResize();
+});
+
+function updateFishSize(){
+
+    const fishBody = document.querySelector(".fish-body");
+
+    let maxHeight = 760;
+
+    textareas.forEach((textarea)=>{
+
+        maxHeight += textarea.scrollHeight * 0.05;
+    });
+
+    fishBody.style.minHeight = maxHeight + "px";
+}
+
+console.log("Dynamic Fishbone Ready");
