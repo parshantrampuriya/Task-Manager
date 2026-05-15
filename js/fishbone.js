cd2bd08bb0ad64c927e2075c9065052d1763cdff
parshantrampuriya <parshantrampuriya@gmail.com>
2026-05-15 21:35:42 +0530

    Update fishbone.js

--- js/fishbone.js
+++ js/fishbone.js
@@ -1,14 +1,3 @@
 // FILE NAME: fishbone.js
 
-function updateProblem(){
-
-    const input = document.getElementById("problemInput").value;
-    const problemBox = document.getElementById("problemBox");
-
-    if(input.trim() === ""){
-        alert("Please enter a problem");
-        return;
-    }
-
-    problemBox.innerText = input;
-}
+console.log("Creative Fishbone Diagram Loaded");
