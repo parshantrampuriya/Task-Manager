ebfab536086eb9119b9b7bd0536615b3821a09e0
parshantrampuriya <parshantrampuriya@gmail.com>
2026-05-15 21:52:14 +0530

    Update fishbone.js

--- js/fishbone.js
+++ js/fishbone.js
@@ -1,34 +1,3 @@
-/* REPLACE YOUR COMPLETE fishbone.js WITH THIS */
+// fishbone.js
 
-const textareas = document.querySelectorAll("textarea");
-
-textareas.forEach((textarea)=>{
-
-    function autoResize(){
-
-        textarea.style.height = "auto";
-        textarea.style.height = textarea.scrollHeight + "px";
-
-        updateFishSize();
-    }
-
-    textarea.addEventListener("input",autoResize);
-
-    autoResize();
-});
-
-function updateFishSize(){
-
-    const fishBody = document.querySelector(".fish-body");
-
-    let maxHeight = 760;
-
-    textareas.forEach((textarea)=>{
-
-        maxHeight += textarea.scrollHeight * 0.05;
-    });
-
-    fishBody.style.minHeight = maxHeight + "px";
-}
-
-console.log("Dynamic Fishbone Ready");
+console.log("Professional Fishbone Loaded");
